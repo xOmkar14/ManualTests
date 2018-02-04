eb0a22bcccccb00d888c7c7c3faeadc887443db6
xOmkar14 <omkar_xn4u@yahoo.com>
2018-02-04 13:17:28 +0000

    first commit

---
+++ src/Day_1.java
@@ -0,0 +1,12 @@
+public class Day_1 {
+    public static void main (String [] args) {
+        String greeting = "Hi Hello, How are you";
+        System.out.print(greeting);
+        System.out.print(" My");
+        System.out.print(" Name");
+        System.out.print(" Is");
+        System.out.print(" Omkar");
+        //System.out.println("Hello World");
+        int x; //int y;
+    }
+}
