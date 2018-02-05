fe42f24daee662625facd17ca5b90da22d44942e
xOmkar14 <omkar_xn4u@yahoo.com>
2018-02-05 14:11:53 +0000

    Added more Line to 5

--- src/Day_1.java
+++ src/Day_1.java
@@ -1,5 +1,10 @@
 public class Day_1 {
     public static void main (String [] args){
         System.out.println("1");
-}
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+
+    }
 }
