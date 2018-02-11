0aebe7724a888f44eab957270ef53e946860eebd
xOmkar14 <omkar_xn4u@yahoo.com>
2018-02-11 10:54:19 +0000

    Calculator Homework

--- src/Day_1.java
+++ src/Day_1.java
@@ -1,14 +1,31 @@
 public class Day_1 {
-    public static void main (String [] args){
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
-        System.out.println("6");
-        System.out.println("7");
-        System.out.println("8");
-        System.out.println("9");
-        System.out.println("10");
+
+    public static void changeName(String name){
+        System.out.println(name);
+    //To make name static
+    }
+
+    public static void changeAddress(String address,String contact){
+        System.out.println(address);
+        System.out.println("Mobile no :"+contact);
+    //Can make 2 call outs in 1 string
+    }
+
+    public void changeDOB(String DOB){
+        System.out.println(DOB);
+    //To make DOB static callout
+    }
+
+    public static void main (String [] args) {
+
+        Day_1 day_1 = new Day_1();
+        changeName("Omkar");
+        changeAddress("11 Priory Avenue, Wembley, HA02SB","07590574614");
+        day_1.changeDOB("22-10-1989");
+    // Dont have to use day_1 for all the callouts
+
+        //System.out.println ("My contact number: 07590574614");
+        //System.out.println ("DOB: 22/10/1989");
+
     }
 }
---
+++ src/Maths.java
@@ -0,0 +1,40 @@
+public class Maths {
+
+    //declare here variables int a and b and give values for int
+    int a = 50;
+    int b = 20;
+    int ans;
+
+    public static void main (String[] args) {
+        Maths maths = new Maths();
+        maths.addition();
+        maths.subtraction();
+        maths.multiplication();
+        maths.division();
+    //make it into a object in main method
+    }
+
+    public void addition(){
+       ans = a + b;
+        System.out.println(ans);
+        //for addition
+    }
+
+    public void subtraction(){
+        ans = a - b;
+        System.out.println(ans);
+        //for subtraction
+    }
+
+    public void multiplication(){
+        ans = a * b;
+        System.out.println(ans);
+        //for multiplication
+    }
+
+    public void division(){
+        ans = a / b;
+        System.out.println(ans);
+        //for division
+    }
+}
